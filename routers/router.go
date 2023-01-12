e311cda3aca2bb3d95e96194bb1386b4776bb178
zguo14 <guozihao97@outlook.com>
2023-01-12 16:37:22 -0500

    fix

--- routers/router.go
+++ routers/router.go
@@ -12,6 +12,20 @@ func InitRouter() *gin.Engine {
 
 	r.Use(gin.Recovery())
 
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	gin.SetMode(settings.RunMode)
 
 	apiv1 := r.Group("/api/v1")
